a6c69de8bbba421f6a6c77f52bdb9ac9203adc80
Dmytro Shchepinskyi <dimaschep@gmail.com>
2023-10-28 15:43:49 +0200

    game page add

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ControlBar from "@/components/ControlBar";
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center pt-4">
       <div className="border h-1/2 w-1/2 grid grid-rows-[20px_minmax(0px,_1fr)]">
         <ControlBar />
         <AnimatedText />
---
+++ app/playGame/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GamePage = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="h-3/4 w-11/12 border mt-4">GamePage</div>
+    </div>
+  );
+};
+
+export default GamePage;
